2747733cb87ef7d5f7790e4358b5b95652bf6762
celilguvensen <cllahg@gmail.com>
2023-03-20 17:01:12 +0300

    postgre database change

--- database.py
+++ database.py
@@ -3,8 +3,15 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread": False})
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread": False})
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind= engine)
+# Base = declarative_base()
+
+
+SQLALCHEMY_DATABASE_URL = "postgresql://ndlvgrnp:6PmHoKfOUWKOBw2omx9_BSGnDMyYVkH1@mahmud.db.elephantsql.com/ndlvgrnp"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind= engine)
 Base = declarative_base()
